08ff998b13baaa354e52a60d2b7e45b978f98969
Danielle Roberson <danielle.roberson@icloud.com>
2017-03-22 12:58:32 -0700

    moving files from old rep that didn't work

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,49 @@
-// YOUR SCRIPTS GO HERE
+// Randomize Header
+
+// Store & Generate Number
+var maxRand = 3;
+var numRand = Math.floor( Math.random() * maxRand );
+//console.log(numRand);
+
+// Store images
+var img0 = '/img/fullsize/1.jpg';
+var img1 = '/img/fullsize/2.jpg';
+var img2 = '/img/fullsize/3.jpg';
+
+// Show Images
+$('header img').attr('src', eval('img' + numRand) );
+
+// Activate Gallery
+$('figure').lightGallery({
+    selector: 'a',
+    thumbnail: false,
+    
+    
+});
+
+
+//Smooth Scrolling
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1500, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
